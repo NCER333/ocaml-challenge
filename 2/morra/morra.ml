73e9936287002205ba69e91c4f7d1d54283e3a32
NCER333 <florealessio54@gmail.com>
2025-10-14 18:41:31 +0200

    esercizio morra  Please enter the commit message for your changes. Lines starting

---
+++ 2/morra/morra.ml
@@ -0,0 +1,9 @@
+type winner = Player | Computer | Tie ;;
+
+let win (hp, gp) = 
+  let hc = Random.int(6) in
+  let gc = Random.int(11) in
+  if ((gp = hp + hc) && (gc != hp + hc)) then ( hc, gc, Player) else
+    if(gc = hp + hc) then ((hc, gc, Computer)) else
+      (hc, gc, Tie);;
+
